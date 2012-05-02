5bdf9f48e348e59a8cc4dba0c779b2ceb4c496b6
aotis <imass82@gmail.com>
2012-05-02 00:18:04 -0700

    All buttons now function except the join button and the help/credits button.
    Problem with InputLabel was found. Fixed in next release, but xna 4.0 compatable only
    Radio buttons for teams added.

--- AngryTanks.Client/JoinMenu.cs
+++ AngryTanks.Client/JoinMenu.cs
@@ -29,21 +29,22 @@ namespace AngryTanks.Client
     public class JoinMenu : GameState
     {
         private Game game; // needed to load textures
-        GameStateManager gameStateManager;  // stores the gameStateManager used in AngryTanks
+        private GameStateManager gameStateManager;  // stores the gameStateManager used in AngryTanks
                                             // this allows switching to new gamestates from within a gamestate
-        GuiManager gui;
-        InputManager input;
-        GameServiceContainer gameService;
-        Texture2D bg;
+        private GuiManager gui;
+        private InputManager input;
+        private GameServiceContainer gameService;
+        private Texture2D bg;
 
         private SpriteBatch spriteBatch; // passing spriteBatch from AngryTanks didn't work
-        KeyboardState keyboardState, oldKeyboardState;
+        
         
         private Screen joinScreen;
         private LabelControl callsignLabel, tagLabel, hostLabel, teamLabel;
         private ButtonControl joinButton, backButton;
         private InputControl callsignInput, tagInput, hostInput;
-        private ChoiceControl teamChoice;
+        private Control teamChoice;
+        private ChoiceControl redTeam, blueTeam, greenTeam;
 
 
         public JoinMenu(Game game, GameStateManager gameStateManager, GuiManager gui, InputManager input)
@@ -65,7 +66,11 @@ namespace AngryTanks.Client
             joinScreen.Desktop.Bounds = new UniRectangle(new UniScalar(0.0f, 0.0f), new UniScalar(0.0f, 0.0f),
                                                         new UniScalar(1.0f, 0.0f), new UniScalar(1.0f, 0.0f));
             InitializeComponents();
+
+            joinButton.Pressed += new EventHandler(joinButton_Pressed);
+            backButton.Pressed += new EventHandler(backButton_Pressed);
         }
+
         public override void Initialize()
         {
             
@@ -79,14 +84,7 @@ namespace AngryTanks.Client
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyUp(Keys.Escape) && oldKeyboardState.IsKeyDown(Keys.Escape))
-                gameStateManager.Switch(new MainMenu(game, gameStateManager, gui, input));
-
-           
-
-            oldKeyboardState = keyboardState;
-
+            
             base.Update(gameTime);
         }
 
@@ -130,7 +128,11 @@ namespace AngryTanks.Client
 
             this.joinButton = new ButtonControl();
             this.backButton = new ButtonControl();
-            this.teamChoice = new ChoiceControl();
+            this.teamChoice = new Control();
+
+            this.redTeam = new ChoiceControl();
+            this.blueTeam = new ChoiceControl();
+            this.greenTeam = new ChoiceControl();
 
             this.callsignLabel.Text = "Callsign: ";
             this.callsignLabel.Bounds = new UniRectangle(new UniScalar(.7f, 0f), new UniScalar(.2f, 0f), 50, 24);
@@ -153,13 +155,26 @@ namespace AngryTanks.Client
             this.teamLabel.Text = "Team: ";
             this.teamLabel.Bounds = new UniRectangle(new UniScalar(0.7f, 0f), new UniScalar(0.5f, 0f), 50, 24);
 
+            this.teamChoice.Bounds = new UniRectangle(new UniScalar(0.7f, 0f), new UniScalar(0.5f, 0f), 150, 94);
+
+            this.redTeam.Text = "Red Team";
+            this.redTeam.Bounds = new UniRectangle(new UniScalar(0.0f, 80f), new UniScalar(0.0f, 10f), 10, 10);
+            this.redTeam.Enabled = true;
+
+            this.blueTeam.Text = "Blue Team";
+            this.blueTeam.Bounds = new UniRectangle(new UniScalar(0.0f, 80f), new UniScalar(0.0f, 30f), 10, 10);
+            this.blueTeam.Enabled = true;
+
+            this.greenTeam.Text = "Green Team";
+            this.greenTeam.Bounds = new UniRectangle(new UniScalar(0.0f, 80f), new UniScalar(0.0f, 50f), 10, 10);
+            this.greenTeam.Enabled = true;
 
             this.joinButton.Text = "Join!";
             this.joinButton.Bounds = new UniRectangle(new UniScalar(.7f, 80f), new UniScalar(0.7f, 0f), 150, 72);
             // 80, 24 for regular button
 
             this.backButton.Text = "Back";
-            this.backButton.Bounds = new UniRectangle(new UniScalar(0.2f, 0.0f), new UniScalar(0.9f, 0f), 80, 24);
+            this.backButton.Bounds = new UniRectangle(new UniScalar(0.1f, 0.0f), new UniScalar(0.9f, 0f), 80, 24);
 
             joinScreen.Desktop.Children.Add(callsignLabel);
             joinScreen.Desktop.Children.Add(callsignInput);
@@ -168,8 +183,24 @@ namespace AngryTanks.Client
             joinScreen.Desktop.Children.Add(hostLabel);
             joinScreen.Desktop.Children.Add(hostInput);
             joinScreen.Desktop.Children.Add(teamLabel);
+            joinScreen.Desktop.Children.Add(teamChoice);
             joinScreen.Desktop.Children.Add(joinButton);
             joinScreen.Desktop.Children.Add(backButton);
+
+            teamChoice.Children.Add(redTeam);
+            teamChoice.Children.Add(blueTeam);
+            teamChoice.Children.Add(greenTeam);
+        }
+
+        void backButton_Pressed(object sender, EventArgs e)
+        {
+            gameStateManager.Switch(new MainMenu(game, gameStateManager, gui, input));
+        }
+
+        void joinButton_Pressed(object sender, EventArgs e)
+        {
+            // not yet implemented
+            return;
         }
     }
 }
--- AngryTanks.Client/MainMenu.cs
+++ AngryTanks.Client/MainMenu.cs
@@ -31,19 +31,12 @@ namespace AngryTanks.Client
         private GameStateManager gameStateManager;  // stores refernece to the GameStateManager used in AngryTanks
                                             // this allows switching to new gamestates from within a gamestate
         private GuiManager gui; // stores reference to the GuiManager
-        private InputManager input;
+        private InputManager input; // stores reference to the InputManager
         private GraphicsDevice graphicsDevice; // needed for spriteBatch
         private SpriteBatch spriteBatch; // stores reference to SpriteBatch
-        /* not needed 
-        private KeyboardState keyboardState, oldKeyboardState;
-
-        private enum buttons { join, help, quit } // an enum for each of the button selections
-        private int highlightedButton; // to be used as an index for the enum buttons
-        private Color[] buttonColor; // using an array instead of List for direct access
-        */
 
         private Screen mainMenuScreen; // screen to be used by the guimanager for this object
-        private ButtonControl joinButton, helpButton, quitButton;
+        private ButtonControl joinButton, helpButton, quitButton; // the buttons
 
         public MainMenu(Game game, GameStateManager gameStateManager, GuiManager gui, InputManager input)
             : base(gameStateManager)
@@ -51,11 +44,6 @@ namespace AngryTanks.Client
             // load textures
             // this can probably go in OnEntered()
             bg = game.Content.Load<Texture2D>("textures/menu/greentank");
-            /*
-            join = game.Content.Load<Texture2D>("textures/menu/join");
-            help = game.Content.Load<Texture2D>("textures/menu/help");
-            quit = game.Content.Load<Texture2D>("textures/menu/quit");
-             */
 
             // stays in constructor
             this.gameStateManager = gameStateManager;
@@ -67,15 +55,6 @@ namespace AngryTanks.Client
             graphicsDevice = game.GraphicsDevice;
             spriteBatch = new SpriteBatch(graphicsDevice);
             
-            /*
-            highlightedButton = (int)buttons.join;
-
-            buttonColor = new Color[3];
-            buttonColor[0] = Color.Yellow;
-            buttonColor[1] = Color.White;
-            buttonColor[2] = Color.White;
-             */
-
             Viewport viewport = game.GraphicsDevice.Viewport;
             mainMenuScreen = new Screen(viewport.Width, viewport.Height);
 
@@ -83,7 +62,15 @@ namespace AngryTanks.Client
             mainMenuScreen.Desktop.Bounds = new UniRectangle(new UniScalar(0.0f, 0.0f), new UniScalar(0.0f, 0.0f),
                                                         new UniScalar(1.0f, 0.0f), new UniScalar(1.0f, 0.0f));
             InitializeComponents();
+
+            joinButton.Pressed += new EventHandler(joinButton_Pressed);
+            helpButton.Pressed += new EventHandler(helpButton_Pressed);
+            quitButton.Pressed += new EventHandler(quitButton_Pressed);
         }
+
+        
+
+        
 
         /// <summary>
         /// Allows the game component to update itself.
@@ -91,31 +78,7 @@ namespace AngryTanks.Client
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            /* not needed 
-            keyboardState = Keyboard.GetState();
             
-            // I'm still not familuar with delagates, so I didn't use them
-            // changes the selected button using the arrow keys
-            if (keyboardState.IsKeyUp(Keys.Down) && oldKeyboardState.IsKeyDown(Keys.Down))
-                changeHighlightedButton(1);
-            else if(keyboardState.IsKeyUp(Keys.Up) && oldKeyboardState.IsKeyDown(Keys.Up))
-                changeHighlightedButton(-1);
-
-            // when enter is pressed, the gamestate changes to the selected option
-            if (keyboardState.IsKeyUp(Keys.Enter) && oldKeyboardState.IsKeyDown(Keys.Enter))
-            {
-                if (highlightedButton == (int)buttons.join)
-                    gameStateManager.Switch(new JoinMenu(game, gameStateManager, gui));
-                if (highlightedButton == (int)buttons.help)
-                    // TODO: switch to help screen
-                // also exits the game
-                if(highlightedButton == (int)buttons.quit)
-                    game.Exit();
-             
-            }
-
-            oldKeyboardState = keyboardState;
-            */
             base.Update(gameTime);
         }
 
@@ -131,37 +94,6 @@ namespace AngryTanks.Client
                             1.0f,
                             SpriteEffects.None,
                             1.0f);
-            /*
-            spriteBatch.Draw(join,
-                            new Vector2(game.Window.ClientBounds.Width * .75f, (game.Window.ClientBounds.Height * .75f) -
-                                                                                                    join.Height),
-                            null,
-                            buttonColor[(int)buttons.join],
-                            0.0f,
-                            new Vector2(join.Width / 2, join.Height / 2),
-                            1.0f,
-                            SpriteEffects.None,
-                            0.0f);
-            spriteBatch.Draw(help,
-                            new Vector2(game.Window.ClientBounds.Width * .75f, (game.Window.ClientBounds.Height * .75f)),
-                            null,
-                            buttonColor[(int)buttons.help],
-                            0.0f,
-                            new Vector2(help.Width / 2, help.Height / 2),
-                            1.0f,
-                            SpriteEffects.None,
-                            0.0f);
-            spriteBatch.Draw(quit,
-                            new Vector2(game.Window.ClientBounds.Width * .75f, (game.Window.ClientBounds.Height * .75f) +
-                                                                                                    26),
-                            null,
-                            buttonColor[(int)buttons.quit],
-                            0.0f,
-                            new Vector2(quit.Width / 2, quit.Height / 2),
-                            1.0f,
-                            SpriteEffects.None,
-                            0.0f);
-             */
             spriteBatch.End();
         }
 
@@ -172,19 +104,7 @@ namespace AngryTanks.Client
         protected override void OnPause() { }
 
         protected override void OnResume() { }
-        /* not needed
-        // updates members in array with correct tint
-        protected void changeHighlightedButton(int direction)
-        {
-            buttonColor[highlightedButton] = Color.White;
-                highlightedButton += direction;
-                if (highlightedButton > (int)buttons.quit)
-                    highlightedButton = (int)buttons.join;
-                else if (highlightedButton < (int)buttons.join)
-                    highlightedButton = (int)buttons.quit;
-            buttonColor[highlightedButton] = Color.Yellow;
-        }
-         */
+        
         protected void InitializeComponents()
         {
             joinButton = new ButtonControl();
@@ -204,5 +124,21 @@ namespace AngryTanks.Client
             mainMenuScreen.Desktop.Children.Add(helpButton);
             mainMenuScreen.Desktop.Children.Add(quitButton);
         }
+        void joinButton_Pressed(object sender, EventArgs e)
+        {
+            gameStateManager.Switch(new JoinMenu(game, gameStateManager, gui, input));
+
+        }
+        void helpButton_Pressed(object sender, EventArgs e)
+        {
+            // not implemented yet
+            return;
+        }
+
+        void quitButton_Pressed(object sender, EventArgs e)
+        {
+            game.Exit();
+        }
+
     }
 }
